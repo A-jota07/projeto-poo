a2130e72d809de11709c41e1134a401bfda5ea8b
Alexandre Cássio de Souza Junior <souzajunioralexandre340@gmail.com>
2025-11-14 10:09:52 -0400

    Template_method.

---
+++ template_method_1.py
@@ -0,0 +1,42 @@
+from abc import ABC, abstractmethod
+
+
+class Abstract(ABC):
+    def template_method(self):
+        self.operation1()
+        self.base_class_method()
+        self.operation2()
+
+    def base_class_method(self):
+        print('Ola eu sou da classe abstrata')
+
+    @abstractmethod
+    def operation1(self): pass
+
+    @abstractmethod
+    def operation2(self): pass
+
+
+class ConcreteClass1(Abstract):
+    def operation1(self):
+        print('Operação 1 concluida')
+
+    def operation2(self):
+        print('Operação 2 concluida')
+
+
+class ConcreteClass2(Abstract):
+    def operation1(self):
+        print('Operação 1 concluida (Maneira diferente)')
+
+    def operation2(self):
+        print('Operação 2 concluida (Maneira diferente)')
+
+
+if __name__ == '__main__':
+    c1 = ConcreteClass1()
+    c1.template_method()
+    print()
+
+    c2 = ConcreteClass2()
+    c2.template_method()
